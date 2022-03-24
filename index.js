80f0f771f36b3f527ee5fe29313125ae2dcb1d60
Destyni <Destyninjohnson20@icloud.com>
2022-03-24 18:05:49 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer() {
+    if (setBestCustomer) {
+        bestCustomer = 'not bob'
+    }
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    if (overwriteBestCustomer) {
+        bestCustomer = 'maybe bob'
+    }
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'brent'
+
+function changeLeastFavoriteCustomer() {
+    if (changeLeastFavoriteCustomer) {
+        leastFavoriteCustomer = 'donnie'
+    }
+    return leastFavoriteCustomer
+}
